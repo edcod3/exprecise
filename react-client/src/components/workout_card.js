5667d6399513b3894dd5843ebf73778bfe9967fc
edcod3 <31140082+edcod3@users.noreply.github.com>
2021-03-04 23:18:06 +0100

    more code refactoring (react)

--- react-client/src/components/workout_card.js
+++ react-client/src/components/workout_card.js
@@ -1,48 +1,55 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Card from "@material-ui/core/Card"
+import CardContent from "@material-ui/core/CardContent"
+import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles({
-    root: {
-        marginBottom: 20, 
-        minWidth: 275,
-        marginRight: 32
-    },
-    bullet: {
-      display: 'inline-block',
-      margin: '0 2px',
-      transform: 'scale(0.8)',
-    },
-    title: {
-      paddingBottom: 10
-    },
-    pos: {
-        marginTop: 5,
-        marginBottom: 12,
-        fontSize: 14
-    },
-  });
-  
+	root: {
+		marginBottom: 20,
+		minWidth: 275,
+		marginRight: 32
+	},
+	bullet: {
+		display: "inline-block",
+		margin: "0 2px",
+		transform: "scale(0.8)"
+	},
+	title: {
+		paddingBottom: 10
+	},
+	pos: {
+		marginTop: 5,
+		marginBottom: 12,
+		fontSize: 14
+	}
+})
 
 export default function WOcard(props) {
-    const classes = useStyles();
+	const classes = useStyles()
 
-    return (
-        <Card className={classes.root} variant="outlined">
-        <CardContent>
-          <Typography variant="h5" component="h2" className={classes.title}>
-            {props.data.title}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {props.data.desc}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            <p>Reps: {(props.data.weight) ?  props.data.weight : "None"}</p>
-            <p>Weight: {(props.data.weight) ?  props.data.weight + "kg" : "None"}</p>
-          </Typography>
-        </CardContent>
-      </Card>
-    )
+	return (
+		<Card className={classes.root} variant="outlined">
+			<CardContent>
+				<Typography
+					variant="h5"
+					component="h2"
+					className={classes.title}>
+					{props.data.title}
+				</Typography>
+				<Typography variant="body2" component="p">
+					{props.data.desc}
+				</Typography>
+				<Typography className={classes.pos} color="textSecondary">
+					<p>
+						Reps: {props.data.weight ? props.data.weight : "None"}
+					</p>
+					<p>
+						Weight:{" "}
+						{props.data.weight ? props.data.weight + "kg" : "None"}
+					</p>
+				</Typography>
+			</CardContent>
+		</Card>
+	)
 }
--- react-client/src/components/workouts.js
+++ react-client/src/components/workouts.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from "react"
-import { gql } from '@apollo/client'
-import {client} from '../utils/apollo'
-import WOcard from './workout_card'
+import { gql } from "@apollo/client"
+import { client } from "../utils/apollo"
+import WOcard from "./workout_card"
 
 export default function Home() {
-	const [apiData, setapiData] = useState([{title:"No Workouts", desc:"No Data loaded", reps: null, weight: null}])
+	const [apiData, setapiData] = useState([
+		{
+			title: "No Workouts",
+			desc: "No Data loaded",
+			reps: null,
+			weight: null
+		}
+	])
 
 	useEffect(() => {
 		console.log("GraphQL-Apollo Client")
 		client
 			.query({
 				query: gql`
-				query {
-					workouts {
-						title
-						desc
-						reps
+					query {
+						workouts {
+							title
+							desc
+							reps
+						}
 					}
-				}
 				`
 			})
-			.then(result => {
+			.then((result) => {
 				console.log(result.data.workouts)
 				setapiData(result.data.workouts)
 			})
@@ -30,7 +37,7 @@ export default function Home() {
 		<div>
 			<h2>Your Workouts</h2>
 			{apiData.map((data) => {
-				return<WOcard data={data} />
+				return <WOcard data={data} />
 			})}
 		</div>
 	)
