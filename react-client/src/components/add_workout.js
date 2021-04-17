7d07319e0568ddf066de0bc3034928c813aa636c
edcod3 <edwinster99@gmail.com>
2021-04-17 16:04:03 +0200

    React-Apollo mutation to add workout

--- react-client/src/components/add_workout.js
+++ react-client/src/components/add_workout.js
@@ -1,5 +1,7 @@
 import React from "react"
-import { useForm } from "react-hook-form"
+import { gql } from "@apollo/client"
+import { client } from "../utils/apollo"
+import { useForm, Controller } from "react-hook-form"
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
 //import Typography from "@material-ui/core/Typography"
@@ -16,11 +18,46 @@ export default function AddWorkout() {
 	const formStyle = AddStyles()
 
 	//React-Hook-Form Stuff
-	const { register, handleSubmit, watch } = useForm()
+	const { handleSubmit, watch, control } = useForm()
+	//const {register } = useForm()
 	//const { errors } = useForm()
-	const onSubmit = (data) => console.log(data)
+	const onSubmit = (data) => {
+		console.log(data)
+		console.log("Title: " + data.title)
+		client
+			.mutate({
+				mutation: gql`
+					mutation AddWorkout(
+						$title: String!
+						$desc: String!
+						$reps: Int!
+						$weight: Int!
+					) {
+						add(
+							title: $title
+							desc: $desc
+							reps: $reps
+							weight: $weight
+						) {
+							title
+							desc
+							reps
+						}
+					}
+				`,
+				variables: {
+					title: data.title,
+					desc: data.description,
+					reps: parseInt(data.reps),
+					weight: parseInt(data.weight)
+				}
+			})
+			.then((result) => {
+				console.log(result.data.workouts)
+			})
+	}
 
-	console.log(watch("example"))
+	//console.log(watch("example"))
 
 	return (
 		<div>
@@ -31,60 +68,104 @@ export default function AddWorkout() {
 					<form
 						onSubmit={handleSubmit(onSubmit)}
 						className={formStyle.root}>
-						<TextField
-							className={formStyle.title}
+						<Controller
 							name="title"
-							label="Title"
-							required
-							variant="outlined"
-							ref={register({ required: true })}
+							as={
+								<TextField
+									className={formStyle.title}
+									id="title"
+									label="Title"
+									required
+									variant="outlined"
+								/>
+							}
+							control={control}
+							defaultValue=""
+							rules={{
+								required: true
+							}}
 						/>
 						{[1, 2].map(() => (
 							<br />
 						))}
-						<TextField
-							className={formStyle.desc}
+						<Controller
 							name="description"
-							variant="outlined"
-							label="Description"
-							multiline
-							rows={4}
-							ref={register({ required: true })}
+							as={
+								<TextField
+									className={formStyle.desc}
+									id="description"
+									variant="outlined"
+									label="Description"
+									multiline
+									rows={4}
+								/>
+							}
+							control={control}
+							defaultValue=""
+							rules={{
+								required: true
+							}}
 						/>
 						{[1, 2].map(() => (
 							<br />
 						))}
 						<div className={formStyle.repkg_wrapper}>
-							<TextField
+							<Controller
 								name="reps"
-								className={formStyle.rep_kg}
-								label="Reps"
-								variant="outlined"
-								type="number"
-								InputProps={{
-									endAdornment: (
-										<InputAdornment position="start">
-											x
-										</InputAdornment>
-									)
+								as={
+									<TextField
+										id="reps"
+										className={formStyle.rep_kg}
+										label="Reps"
+										variant="outlined"
+										type="number"
+										InputProps={{
+											endAdornment: (
+												<InputAdornment position="start">
+													x
+												</InputAdornment>
+											)
+										}}
+									/>
+								}
+								control={control}
+								defaultValue=""
+								rules={{
+									required: true,
+									pattern: {
+										value: /[0-9]/i,
+										message: "Rep count is invalid"
+									}
 								}}
-								ref={register({ min: 0 })}
 							/>
-							<TextField
+							<Controller
 								name="weight"
-								className={formStyle.rep_kg}
-								label="Weight"
-								variant="outlined"
-								type="number"
-								min="0"
-								InputProps={{
-									endAdornment: (
-										<InputAdornment position="start">
-											kg
-										</InputAdornment>
-									)
+								as={
+									<TextField
+										id="weight"
+										className={formStyle.rep_kg}
+										label="Weight"
+										variant="outlined"
+										type="number"
+										min="0"
+										InputProps={{
+											endAdornment: (
+												<InputAdornment position="start">
+													kg
+												</InputAdornment>
+											)
+										}}
+									/>
+								}
+								control={control}
+								defaultValue=""
+								rules={{
+									required: true,
+									pattern: {
+										value: /[0-9]/i,
+										message: "Weight count is invalid"
+									}
 								}}
-								ref={register({ min: 0 })}
 							/>
 						</div>
 						{[1, 2].map(() => (
